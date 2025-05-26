0b96dcf639e1912be1a1818566d1cb9ad201f4fa
Imanmysarah <imanmysarah3108@gmail.com>
2025-05-26 22:12:47 +0800

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,67 +7,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: ExerciseOne(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class ExerciseOne extends StatelessWidget {
+  // Only 5 image paths
+  final List<String> imagePaths = [
+    'assets/images/flower1.jpg',
+    'assets/images/flower2.jpg',
+    'assets/images/flower3.jpg',
+    'assets/images/flower4.jpg',
+    'assets/images/flower5.jpeg',
+  ];
+  final List<String> imagePaths2 = [
+    'assets/images/gift1.jpg',
+    'assets/images/gift2.jpg',
+    'assets/images/gift3.jpg',
+    'assets/images/gift4.jpg',
+    'assets/images/gift5.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Exercise 1'), // title: Text
-        leading: Icon(Icons.menu), // leading: Icon
-        actions: [ // actions: IconButton
+        title: Text('Pure Gift'),
+        leading: Icon(Icons.menu),
+        actions: [
           IconButton(
             icon: Icon(Icons.more_vert),
             onPressed: () {},
           ),
         ],
-        backgroundColor: const Color.fromARGB(255, 251, 182, 210),
+        backgroundColor: const Color.fromARGB(255, 252, 204, 242),
       ),
       body: Column(
         children: [
-          /// Container with horizontal ListView
+          /// Horizontal ListView (5 boxes)
           Container(
-            height: 120,
-            color: const Color.fromARGB(255, 255, 194, 235),
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 10),
             child: ListView.builder(
               scrollDirection: Axis.horizontal,
-              itemCount: 10,
+              itemCount: imagePaths.length,
               itemBuilder: (context, index) {
                 return Container(
-                  width: 120,
+                  width: 100,
                   margin: EdgeInsets.all(8),
                   decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
-                        blurRadius: 8,
-                        color: const Color.fromARGB(250, 3, 3, 3).withOpacity(0.5),
+                        blurRadius: 4,
+                        color: const Color.fromARGB(255, 19, 19, 19).withOpacity(0.3),
                         offset: Offset(0, 2),
-                      ),
-                    ],
-                    color: const Color.fromARGB(255, 255, 255, 255),
+                      )],
                     borderRadius: BorderRadius.circular(10),
                     image: DecorationImage(
-                      image: NetworkImage('https://picsum.photos/100/100?random=$index'),                 
+                      image: AssetImage(imagePaths[index]),
                       fit: BoxFit.cover,
                     ),
                   ),
                   alignment: Alignment.bottomCenter,
                   child: Container(
                     padding: EdgeInsets.all(4),
+                    color: const Color.fromARGB(0, 255, 255, 255),
                     child: Text(
-                      'Item ${index + 1}',
+                      'Bouquet ${index + 1}',
                       style: TextStyle(
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                          shadows: [
+                        color: Colors.white,
+                        shadows: [
                           Shadow(
                             offset: Offset(1.0, 1.0),
                             blurRadius: 2.0,
                             color: Colors.black,
-                                ),
-                                  ],),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -75,20 +92,20 @@ class ExerciseOne extends StatelessWidget {
             ),
           ),
 
-          /// Expanded with vertical ListView
+          /// Vertical ListView (5 boxes)
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: imagePaths.length,
               itemBuilder: (context, index) {
                 return Container(
                   margin: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(12),
-                    color: const Color.fromARGB(255, 255, 214, 241),
+                    color: Colors.white,
                     boxShadow: [
                       BoxShadow(
-                        blurRadius: 8,
-                        color: const Color.fromARGB(250, 3, 3, 3).withOpacity(0.5),
+                        blurRadius: 4,
+                        color: const Color.fromARGB(255, 19, 19, 19).withOpacity(0.3),
                         offset: Offset(0, 2),
                       ),
                     ],
@@ -98,8 +115,8 @@ class ExerciseOne extends StatelessWidget {
                     children: [
                       ClipRRect(
                         borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
-                        child: Image.network(
-                          'https://picsum.photos/400/200?random=$index',
+                        child: Image.asset(
+                          imagePaths2[index],
                           height: 150,
                           width: double.infinity,
                           fit: BoxFit.cover,
@@ -108,8 +125,18 @@ class ExerciseOne extends StatelessWidget {
                       Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text(
-                          'Title ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                          'Gift Set ${index + 1}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(0.5, 0.5),
+                                blurRadius: 1.5,
+                                color: Colors.black38,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ],
